5c74bd626b6683bc045934a7181af7f4979e28c8
Soah Fernandes <soah.fernandes2005@gmail.com>
2025-11-14 01:54:30 +0530

    If life were unfair we all would be hanging on a cross

--- scripts/run-simple.ps1
+++ scripts/run-simple.ps1
@@ -15,14 +15,14 @@ try {
   if (-not (Test-Path $outDir)) { New-Item -ItemType Directory -Path $outDir | Out-Null }
 
   Write-Host "Compiling..."
-    & javac --module-path "$JFX_LIB" --add-modules javafx.controls,javafx.fxml `
+  & javac --module-path "$JFX_LIB" --add-modules javafx.controls,javafx.fxml `
       -cp "$H2_JAR" `
       -d "$outDir" `
       Produce.java `
       SimpleProduceApp.java
 
   Write-Host "Running..."
-    & java --module-path "$JFX_LIB" --add-modules javafx.controls,javafx.fxml `
+  & java --module-path "$JFX_LIB" --add-modules javafx.controls,javafx.fxml `
       -cp "$outDir;$H2_JAR" `
       SimpleProduceApp
 }
